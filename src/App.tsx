f9317ce9c87fe63a61df29a8380f56d9dcc009c4
Matheus Kuster <matheuskuster@hotmail.com>
2020-08-29 11:28:36 -0300

    Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+/**
+ * IMPORTS
+ */
 import React from 'react';
+import GlobalStyles from 'src/styles/GlobalStyles';
 
+/**
+ * CODE
+ */
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <h1>Hello World</h1>
-        </div>
+        <>
+            <GlobalStyles />
+            <div className="App">
+                <h1>MyApply</h1>
+            </div>
+        </>
     );
 }
 
+/**
+ * EXPORTS
+ */
 export default App;

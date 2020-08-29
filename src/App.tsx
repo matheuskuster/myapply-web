5a4df322a955634b986f696a7a0b859502470240
Matheus Kuster <matheuskuster@hotmail.com>
2020-08-29 11:01:14 -0300

    Add linting and ts config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+
+function App(): JSX.Element
+{
+    return (
+        <div className="App">
+            <h1>Hello World</h1>
+        </div>
+    );
 }
 
 export default App;
